c834808fd5dece911f0995325d18f381f06ee084
Anh Tuan Nguyen <anh.tuan.nguyen@terralogic.com>
2017-03-23 16:17:41 +0700

    First commit

---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Anh Tuan Nguyen on 3/23/17.
+//  Copyright © 2017 com.Project36. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    var imageURL: URL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+        imageURL = DemoURL.Stanford
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
+        }
+    }
+    
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+
+}
