91528ae8b09a39f2e4682fd36fd422aa4b7356a3
Anh Tuan Nguyen <anh.tuan.nguyen@terralogic.com>
2017-03-24 17:08:10 +0700

    Completed Lecture 8

--- Cassini/DemoURL.swift
+++ Cassini/DemoURL.swift
@@ -11,17 +11,19 @@ struct DemoURL
 {
     static let Stanford = URL(string: "http://stanford.edu/about/images/intro_about.jpg")
 
-    static let NASA = [
-        "Cassini" : "http://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg",
-        "Earth" : "http://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg",
-        "Saturn" : "http://www.nasa.gov/sites/default/files/saturn_collage.jpg"
-    ]
-    
-    static func NASAImageNamed(imageName: String?) -> NSURL? {
-        if let urlstring = NASA[imageName ?? ""] {
-            return NSURL(string: urlstring)
-        } else {
-            return nil
+    static var NASA: Dictionary<String, URL> = {
+        let NASAURLStrings = [
+                "Cassini" : "http://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg",
+                "Earth" : "http://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg",
+                "Saturn" : "http://www.nasa.gov/sites/default/files/saturn_collage.jpg"
+        ]
+        var urls = Dictionary<String, URL>()
+        for(key, value) in NASAURLStrings {
+            urls[key] = URL(string: value)
         }
-    }
+        return urls
+    }()
+        
 }
+// http://vignette2.wikia.nocookie.net/heman/images/3/36/Earth.jpg/revision/latest?cb=20130912205625
+// http://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg
